84ac25f3f9d1b727781f409bef51b43906f793a0
Denis <onemanband67@gmail.com>
2020-10-03 18:41:23 +0300

    Initial commit

---
+++ StockProfitCount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockProfitCount
+//
+//  Created by Balakirev, Denis on 03.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
